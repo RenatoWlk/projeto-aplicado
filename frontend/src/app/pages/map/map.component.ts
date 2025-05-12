1269c4759a48f120f025d0f855397bc9ca698135
unknown <enzo_fischer10@hotmail.com>
2025-05-12 18:15:52 -0300

    Upando codigo do mapa, reaproveitado do github projetoIV -sem teste

--- frontend/src/app/pages/map/map.component.ts
+++ frontend/src/app/pages/map/map.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
   standalone: true,
-  imports: [],
   templateUrl: './map.component.html',
-  styleUrl: './map.component.scss'
+  styleUrls: ['./map.component.scss']
 })
-export class MapComponent {
+export class MapComponent implements AfterViewInit {
+  private map: L.Map | undefined;
 
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [-23.55052, -46.633308], // Ex: São Paulo
+      zoom: 13
+    });
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; OpenStreetMap contributors'
+    }).addTo(this.map);
+
+    L.marker([-23.55052, -46.633308]).addTo(this.map)
+      .bindPopup('Você está aqui!')
+      .openPopup();
+  }
 }
