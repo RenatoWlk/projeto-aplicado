af693723b3269e54543abced9e004d646f7c35ae
Pedro Barboza <p_pedrobarboza@hotmail.com>
2025-05-13 18:37:42 -0300

    questionnaire v1

--- frontend/src/app/pages/questionnaire/questionnaire.component.ts
+++ frontend/src/app/pages/questionnaire/questionnaire.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-questionnaire',
-  standalone: true,
-  imports: [],
   templateUrl: './questionnaire.component.html',
-  styleUrl: './questionnaire.component.scss'
+  styleUrls: ['./questionnaire.component.scss']
 })
-export class QuestionnaireComponent {
 
+export class QuestionnaireComponent {
+  dados = {
+    age: null,
+    weight: null,
+    health: '',
+    sleep: '',
+    food: '',
+    id: '',
+    vaccine: '',
+    disease: ''
+  };
+
+  verificarElegibilidade() {
+    const { age, weight, health, sleep, food, id, vaccine, disease } = this.dados;
+
+    if (
+      age >= 16 &&
+      age <= 69 &&
+      weight > 50 &&
+      health === 'yes' &&
+      sleep === 'yes' &&
+      food === 'no' &&
+      id === 'yes' &&
+      vaccine === 'yes' &&
+      disease === 'no'
+    ) {
+      Swal.fire('Você está elegível para doar sangue.');
+    } else {
+      Swal.fire('Que pena!\nVocê está inelegível para doar sangue.');
+    }
+  }
+
+  sair() {
+    // Aqui você pode redirecionar via Angular ou simular logout
+    window.location.href = '/tela-login/telalogin.html';
+  }
 }
