ce897f818117ed9224d702588ac2d0ebbd0aea67
Gabriel Trindade <gtrindade61@gmail.com>
2025-05-16 06:17:25 -0300

    Added material theme and login.component

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.loginForm = this.fb.group({
+      login: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  login() {
+    const { login, password } = this.loginForm.value;
+    console.log('Login:', login, 'Password:', password);
+  }
+
+  goToRegister() {
+    this.router.navigate(['/register']);
+  }
 }
