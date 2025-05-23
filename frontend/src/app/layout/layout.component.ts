bfa69f342f42f311c37b81f778e79d6f85fe6aee
Vinicius Paiola <vfpaiola98@gmail.com>
2025-05-22 23:28:27 -0300

    Fixed small issues

--- frontend/src/app/layout/layout.component.ts
+++ frontend/src/app/layout/layout.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { MatSidenav, MatSidenavContainer, MatSidenavContent, MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 import { SubheaderComponent } from './subheader/subheader.component';
@@ -9,7 +8,7 @@ import { SubheaderComponent } from './subheader/subheader.component';
 @Component({
   standalone: true,
   selector: 'app-layout',
-  imports: [HeaderComponent, FooterComponent, MatSidenav, MatSidenavContainer, MatSidenavContent, MatSidenavModule, MatIconModule, RouterModule,
+  imports: [HeaderComponent, FooterComponent, MatIconModule, RouterModule,
     SubheaderComponent,
   ],
   templateUrl: './layout.component.html',
